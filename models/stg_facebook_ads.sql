cc309c81bafce6658f107e12f83bc5bca4208435
Alexander Dodson <alexander.dodson@answerrocket.com>
2022-06-07 19:00:50 -0400

    various tests

--- models/stg_facebook_ads.sql
+++ models/stg_facebook_ads.sql
@@ -1,4 +1,7 @@
 {{ log(var('ad_reporting__facebook_ads_enabled') | as_bool) }}
+{{ log(var('ad_reporting__facebook_ads_enabled')) }}
+{{ log(var('ad_reporting__facebook_ads_enabled') | as_text) }}
+{{ log(tojson(var('ad_reporting__facebook_ads_enabled'))) }}
 {{ config(enabled=var('ad_reporting__facebook_ads_enabled') | as_bool) }}
 
 with base as (
